6d73886915489d012d369f4ea7063cc59a83d553
Jaimie Hart <hartjme@gmail.com>
2022-06-23 16:59:18 -0400

    homepage css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom"
+import { useState, useEffect, useContext } from "react"
+import { UserList } from "../users/UserList"
+import { UserContext } from "../../UserContext";
+import "./Home.css"
 
 export const Home = () => {
+    const { currentUser } = useContext(UserContext)
+
     return <div>
         <br></br>
-        <h1 className="title has-text-centered has-text-weight-bold is-family-primary is-size-1">Museum54</h1>
+        <div className="has-text-centered">
+        <img className="logo" alt="logo home" src={require("/Users/jaimiehart/workspace/server-capstone/Museum54-Client/src/components/images/museum54 logo.jpg")} />
+        <br></br>
+        <br></br>
+        </div>
+        <div className="currentUser has-text-centered has-text-weight-bold has-text-grey-darker">
+       Welcome {currentUser.username}!
+        </div>
+        <br></br>
         <br></br>
         <div className="buttons is-centered">
             <Link to={`/donate/art`}>
